97d203e998ce8347bdd231aefb1496f3c305fe15
ArtaRizki <yuliartarizkinusantoko@gmail.com>
2026-04-30 04:12:57 +0700

    update

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,12 +1,17 @@
 abstract class AppConfig {
   static const gasEndpoint = String.fromEnvironment('GAS_ENDPOINT');
   static const apiToken = String.fromEnvironment('API_TOKEN');
-  static const appLogo = String.fromEnvironment('APP_LOGO', defaultValue: 'assets/icons/hadirin.png');
-  static const appName = String.fromEnvironment('APP_NAME', defaultValue: 'Hadir.in');
+  static const appLogo = String.fromEnvironment(
+    'APP_LOGO',
+    defaultValue: 'assets/icons/sdit.png',
+  );
+  static const appName = String.fromEnvironment(
+    'APP_NAME',
+    defaultValue: 'SD IT AL FAHMI',
+  );
 
   static void validate() {
     assert(gasEndpoint.isNotEmpty, 'GAS_ENDPOINT belum di-set');
     assert(apiToken.isNotEmpty, 'API_TOKEN belum di-set');
   }
 }
- 
