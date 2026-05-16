f3b97200458b694e9f2b93f289793e1daf5938da
ArtaRizki <yuliartarizkinusantoko@gmail.com>
2026-05-16 14:04:26 +0700

    Sync: Mobile Flutter & Laravel API integration completed

--- hadirin_backend/routes/api.php
+++ hadirin_backend/routes/api.php
@@ -15,52 +15,57 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/tenant/register', [TenantController::class, 'register']);
 
 // Tenant-specific routes (via middleware)
+// Nama rute disamakan dengan 'action' yang dikirimkan oleh aplikasi Flutter (menggunakan underscore)
 Route::middleware([\App\Http\Middleware\TenantMiddleware::class])->group(function () {
-    Route::post('/enroll-device', [AuthController::class, 'enrollDevice']);
+    Route::post('/enroll_device', [AuthController::class, 'enrollDevice']);
     
     // Attendance
     Route::post('/absen', [AttendanceController::class, 'absen']);
-    Route::post('/history', [AttendanceController::class, 'getHistory']);
-    Route::post('/cek-status-hari-ini', [AttendanceController::class, 'cekStatusHariIni']);
-    Route::post('/dashboard-stats', [AttendanceController::class, 'dashboardStats']);
-    Route::post('/monthly-report', [AttendanceController::class, 'monthlyReport']);
+    Route::post('/get_history', [AttendanceController::class, 'getHistory']);
+    Route::post('/cek_status_hari_ini', [AttendanceController::class, 'cekStatusHariIni']);
+    Route::post('/dashboard_stats', [AttendanceController::class, 'dashboardStats']);
+    Route::post('/monthly_report', [AttendanceController::class, 'monthlyReport']);
     
     // Config
-    Route::get('/office-config', [TenantController::class, 'getConfig']);
-    Route::post('/office-config', [TenantController::class, 'getConfig']); // mobile legacy app support
+    Route::get('/get_office_config', [TenantController::class, 'getConfig']);
+    Route::post('/get_office_config', [TenantController::class, 'getConfig']); // mobile legacy app support
     
     // Member management
     Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users', [UserController::class, 'index']); // mobile support
-    Route::post('/add-karyawan', [UserController::class, 'store']);
-    Route::delete('/users/{employeeId}', [UserController::class, 'destroy']);
-    Route::post('/delete-karyawan', [UserController::class, 'destroy']); // mobile support
-    Route::post('/register-face', [UserController::class, 'registerFace']);
-    Route::post('/get-face', [UserController::class, 'getFace']);
-    Route::post('/reset-device', [UserController::class, 'resetDevice']);
-    Route::post('/upload-profile-photo', [UserController::class, 'uploadProfilePhoto']);
+    Route::post('/get_all_karyawan', [UserController::class, 'index']); // mobile support
+    Route::post('/add_karyawan', [UserController::class, 'store']);
+    Route::post('/delete_karyawan', [UserController::class, 'destroy']); // mobile support
+    Route::post('/register_face', [UserController::class, 'registerFace']);
+    Route::post('/get_face', [UserController::class, 'getFace']);
+    Route::post('/reset_device', [UserController::class, 'resetDevice']);
+    Route::post('/upload_profile_photo', [UserController::class, 'uploadProfilePhoto']);
 
     // Activities
-    Route::post('/get-jadwal-kegiatan', [ActivityController::class, 'index']);
-    Route::post('/add-jadwal-kegiatan', [ActivityController::class, 'store']);
-    Route::post('/edit-jadwal-kegiatan', [ActivityController::class, 'edit']);
-    Route::post('/absen-kegiatan', [ActivityController::class, 'absen']);
+    Route::post('/get_jadwal_kegiatan', [ActivityController::class, 'index']);
+    Route::post('/add_jadwal_kegiatan', [ActivityController::class, 'store']);
+    Route::post('/edit_jadwal_kegiatan', [ActivityController::class, 'edit']);
+    Route::post('/absen_kegiatan', [ActivityController::class, 'absen']);
 
     // Leaves
-    Route::post('/ajukan-izin', [LeaveController::class, 'store']);
-    Route::post('/get-leave-history', [LeaveController::class, 'history']);
-    Route::post('/get-all-approvals', [LeaveController::class, 'approvals']);
-    Route::post('/update-leave-status', [LeaveController::class, 'updateStatus']);
+    Route::post('/ajukan_izin', [LeaveController::class, 'store']);
+    Route::post('/get_leave_history', [LeaveController::class, 'history']);
+    Route::post('/get_all_approvals', [LeaveController::class, 'approvals']);
+    Route::post('/update_leave_status', [LeaveController::class, 'updateStatus']);
 
     // Ngaji Logs
-    Route::post('/get-kelompok-ngaji', [NgajiController::class, 'groups']);
-    Route::post('/add-kelompok-ngaji', [NgajiController::class, 'storeGroup']);
-    Route::post('/submit-laporan-ngaji', [NgajiController::class, 'storeLog']);
-    Route::post('/get-laporan-ngaji', [NgajiController::class, 'getLogs']);
+    Route::post('/get_kelompok_ngaji', [NgajiController::class, 'groups']);
+    Route::post('/add_kelompok_ngaji', [NgajiController::class, 'storeGroup']);
+    Route::post('/submit_laporan_ngaji', [NgajiController::class, 'storeLog']);
+    Route::post('/get_laporan_ngaji', [NgajiController::class, 'getLogs']);
     
     // Banners
-    Route::post('/get-banners', [BannerController::class, 'index']);
-    Route::post('/add-banner', [BannerController::class, 'store']);
-    Route::post('/edit-banner/{id}', [BannerController::class, 'update']);
-    Route::post('/delete-banner/{id}', [BannerController::class, 'destroy']);
+    Route::post('/get_banners', [BannerController::class, 'index']);
+    Route::post('/add_banner', [BannerController::class, 'store']);
+    Route::post('/edit_banner/{id}', [BannerController::class, 'update']);
+    Route::post('/delete_banner/{id}', [BannerController::class, 'destroy']);
+
+    // Stats & Ayat (New Parity)
+    Route::post('/get_enhanced_stats', [AttendanceController::class, 'enhancedStats']);
+    Route::post('/get_employee_stats', [AttendanceController::class, 'employeeStats']);
+    Route::post('/get_ayat_pilihan', [TenantController::class, 'getAyat']);
 });
